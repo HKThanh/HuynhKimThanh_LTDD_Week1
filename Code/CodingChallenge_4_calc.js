a8dc6bd6abe6fb6eb5bd93d6aed7acdd1b99e78c
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-08-23 19:51:07 +0700

    week1 ex4 part 2 array

--- Code/CodingChallenge_4_calc.js
+++ Code/CodingChallenge_4_calc.js
@@ -18,6 +18,7 @@
 // 1.
 let bill = [275, 40, 430];
 
+// 2.
 for (let i = 0; i < bill.length; i++) {
     let tip = bill[i] >= 50 && bill[i] <= 300 ? bill[i] * 0.15 : bill[i] * 0.2;
     console.log(`The bill was ${bill[i]}, the tip was ${tip}, and the total value ${bill[i] + tip}`);
