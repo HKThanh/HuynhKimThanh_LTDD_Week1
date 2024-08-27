25ef038b196947129ca8de5cb6471d00d6a46b14
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-08-27 14:56:32 +0700

    week1 js ex13 in 50 Challenges Newbie

--- Code/50_Challenge_Newbie_10_to_18.js
+++ Code/50_Challenge_Newbie_10_to_18.js
@@ -13,8 +13,21 @@ function getPositiveNumbers(numbers) {
     return numbers.filter(number => number > 0);
 }
 let numbers2 = [-1, 2, -3, 4, -5];
-console.log(getPositiveNumbers(numbers2));
+// console.log(getPositiveNumbers(numbers2));
 // 13. Find the maximum number in an array of numbers
+let numbers3 = [-2, 0, 58, 49, 30, 60];
+// Cách 1:
+// let max = numbers3[0];
+// for (let i = 1; i < numbers3.length; i++) {
+//     if (max < numbers3[i]) {
+//         max = numbers3[i];
+//     }
+// }
+// console.log(`Max of array: ${max}`);
+// Cách 2:
+let max = Math.max(...numbers3);
+console.log(`Max of array: ${max}`);
+
 // 14. Print the first 10 Fibonacci numbers without recursion
 // 15. Create a function that will find the nth Fibonacci number using recursion
 // 16. Create a function that will return a Boolean specifying if a number is prime
