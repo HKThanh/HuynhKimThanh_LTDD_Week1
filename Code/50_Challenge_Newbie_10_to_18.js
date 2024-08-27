804856ff5fd99dbf67c4253ce7f916602553c41f
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-08-27 14:41:36 +0700

    week1 js ex12 in 50 Challenges Newbie

--- Code/50_Challenge_Newbie_10_to_18.js
+++ Code/50_Challenge_Newbie_10_to_18.js
@@ -5,10 +5,15 @@ let sum = numbers.reduce((acc, curr) => acc + curr, 0);
 
 // 11. Calculate the average of the numbers in an array of numbers
 let average = sum / numbers.length;
-console.log(average);
+// console.log(average);
 
 // 12. Create a function that receives an array of numbers as argument and returns an array
 // containing only the positive numbers
+function getPositiveNumbers(numbers) {
+    return numbers.filter(number => number > 0);
+}
+let numbers2 = [-1, 2, -3, 4, -5];
+console.log(getPositiveNumbers(numbers2));
 // 13. Find the maximum number in an array of numbers
 // 14. Print the first 10 Fibonacci numbers without recursion
 // 15. Create a function that will find the nth Fibonacci number using recursion
